74a6afb9bb439cb333c7a61b67e3e2895ab69aa4
Pedro Magalhães <38951956+pmags@users.noreply.github.com>
2026-02-21 20:12:57 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,29 @@
 basic.showIcon(IconNames.StickFigure)
 music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+let Time = 0
 basic.pause(100)
-basic.clearScreen()
+loops.everyInterval(60000, function () {
+    Time += 1
+})
 basic.forever(function () {
-    while (true) {
-        basic.showIcon(IconNames.Happy)
-        basic.pause(2000)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        basic.pause(100)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    if (Time > 1) {
+        basic.showNumber(Time)
     }
 })
