4b3000196006326f3b95677838709f794cd7eebb
Pedro Magalhães <38951956+pmags@users.noreply.github.com>
2026-02-21 19:47:44 +0000

    totoro piscas os olhos

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+basic.showIcon(IconNames.StickFigure)
+music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+basic.pause(100)
+basic.clearScreen()
 basic.forever(function () {
-	
+    while (true) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(2000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(100)
+    }
 })
