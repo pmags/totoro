5ebe71a426b8604ef61a7733de25450455be1d48
Pedro Magalhães <38951956+pmags@users.noreply.github.com>
2026-03-08 18:54:09 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,39 @@
+function temFome () {
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        # # . # .
+        . # . . .
+        . . . . .
+        `)
+}
+function countdown10s () {
+    // for loops 是由0開始到10 , 而變數會儲存到index 上
+    for (let index = 0; index <= 10; index++) {
+        // 我們需要在micro:bit 上顯示倒數時間, 而index 是由0開始, 所以我們以10和index相減, 得出倒數
+        basic.showNumber(10 - index)
+        // 為了倒數時間準確, 所以用了1000ms (1秒)來控制
+        basic.pause(1000)
+    }
+}
+function tomarBanho () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        # . # . #
+        `)
+}
+function querTomarBanho () {
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        # . # . #
+        `)
+}
 basic.showIcon(IconNames.StickFigure)
 music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
 basic.showLeds(`
@@ -23,7 +59,8 @@ basic.forever(function () {
         . # # # .
         `)
     basic.pause(100)
-    if (Time > 1) {
-        basic.showNumber(Time)
+    while (true) {
+        countdown10s()
+        querTomarBanho()
     }
 })
