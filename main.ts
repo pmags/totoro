8ab01408d891321b2c42aa23957fe99eab4a061b
Pedro Magalhães <38951956+pmags@users.noreply.github.com>
2026-05-02 19:01:32 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,55 @@
+let Time = 0
+let temperatura = 0
+let luz = 0
+if (luz <= 150) {
+    basic.showLeds(`
+        . # # # #
+        . # . . .
+        . # . . .
+        . # . # #
+        . # # # #
+        `)
+}
+if (luz > 150) {
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `)
+    if (temperatura > 20) {
+        basic.showLeds(`
+            . . # # #
+            . . # # #
+            . . # # #
+            . . # # #
+            . . # # #
+            `)
+    } else if (temperatura < 12) {
+        basic.showLeds(`
+            . . # # .
+            . . # # .
+            . # # # .
+            . . # # .
+            . . . . .
+            `)
+    } else {
+        basic.showString("Nice!")
+    }
+}
 basic.showIcon(IconNames.StickFigure)
 music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-let Time = 0
-basic.pause(100)
+basic.clearScreen()
+let casa_de_banho = 0
+let tomar_banho = 0
+let comer = 0
+let dormir = 0
+let jogar = 0
 loops.everyInterval(60000, function () {
     Time += 1
 })
 basic.forever(function () {
-    basic.showIcon(IconNames.Happy)
-    basic.pause(2000)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    basic.pause(100)
-    if (Time > 1) {
-        basic.showNumber(Time)
-    }
+    luz = input.lightLevel()
+    temperatura = input.temperature()
 })
