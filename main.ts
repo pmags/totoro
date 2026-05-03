b8ba57bf3fca5bc52341704f4ec8a11572297b07
Pedro Magalhães <38951956+pmags@users.noreply.github.com>
2026-05-03 16:28:34 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+input.onGesture(Gesture.FreeFall, function () {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.soaring), music.PlaybackMode.InBackground)
+})
+input.onGesture(Gesture.Shake, function () {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . # . . #
+            # . . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.showLeds(`
+            # . . # .
+            . # . . #
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+    jogar = 0
+})
+let jogar = 0
 let temperatura = 0
 let luz = 0
 if (luz <= 150) {
@@ -41,10 +65,10 @@ basic.showIcon(IconNames.StickFigure)
 music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
 basic.clearScreen()
 let casa_de_banho = randint(0, 3)
-let tomar_banho = randint(0, 6)
-let comer = randint(0, 6)
+let tomar_banho = randint(0, 8)
+let comer = randint(0, 5)
 let dormir = 0
-let jogar = randint(0, 4)
+jogar = randint(0, 4)
 loops.everyInterval(60000, function () {
     casa_de_banho += 0.2
     comer += 0.2
@@ -65,23 +89,35 @@ basic.forever(function () {
         . # # # .
         `)
     basic.pause(100)
-    if (dormir == 24) {
-        basic.showLeds(`
-            # . # . #
-            . . # . .
-            # # # # #
-            . # # # #
-            . # # # #
-            `)
-    } else if (comer == 6) {
+    if (casa_de_banho >= 2) {
     	
-    } else if (casa_de_banho == 3) {
-    	
-    } else if (jogar == 4) {
-    	
-    } else if (tomar_banho == 6) {
+    } else if (casa_de_banho >= 3) {
     	
     } else {
+    	
+    }
+    if (comer >= 5) {
+    	
+    } else if (comer >= 6) {
+    	
+    } else {
+    	
+    }
+    if (jogar >= 5) {
+    	
+    } else if (jogar >= 6) {
+    	
+    } else {
+    	
+    }
+    if (tomar_banho >= 8) {
+    	
+    } else if (tomar_banho >= 10) {
+    	
+    } else {
+    	
+    }
+    if (dormir >= 24) {
     	
     }
 })
