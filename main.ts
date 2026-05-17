58b0a359cfa1a266650a48a5d5045d4f3501e5b5
Pedro Magalhães <38951956+pmags@users.noreply.github.com>
2026-05-17 20:52:45 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,52 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerDown), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        # # # # #
+        . . . # .
+        . . # . .
+        . # . . .
+        # # # # #
+        `)
+    basic.clearScreen()
+    esta_acordado = 0
+})
 input.onGesture(Gesture.FreeFall, function () {
     music.play(music.builtinPlayableSoundEffect(soundExpression.soaring), music.PlaybackMode.InBackground)
 })
 input.onGesture(Gesture.Shake, function () {
-    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
-    for (let index = 0; index < 3; index++) {
+    if (esta_acordado == 1) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . # . . #
+                # . . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+            basic.showLeds(`
+                # . . # .
+                . # . . #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        }
+        jogar = 0
+    } else {
         basic.showLeds(`
-            . # . . #
-            # . . # .
             . . . . .
-            # . . . #
+            . # . # .
             . # # # .
-            `)
-        basic.showLeds(`
-            # . . # .
-            . # . . #
-            . . . . .
-            # . . . #
             . # # # .
+            . . # . .
             `)
+        basic.pause(1000)
+        esta_acordado = 1
     }
-    jogar = 0
 })
 let jogar = 0
+let esta_acordado = 0
 let temperatura = 0
 let luz = 0
 if (luz <= 150) {
@@ -64,6 +89,7 @@ if (luz > 150) {
 basic.showIcon(IconNames.StickFigure)
 music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
 basic.clearScreen()
+esta_acordado += 1
 let casa_de_banho = randint(0, 3)
 let tomar_banho = randint(0, 8)
 let comer = randint(0, 5)
@@ -79,45 +105,49 @@ loops.everyInterval(60000, function () {
 basic.forever(function () {
     luz = input.lightLevel()
     temperatura = input.temperature()
-    basic.showIcon(IconNames.Happy)
-    basic.pause(2000)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    basic.pause(100)
-    if (casa_de_banho >= 2) {
-    	
-    } else if (casa_de_banho >= 3) {
-    	
+    if (esta_acordado == 1) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(2000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(100)
+        if (casa_de_banho >= 2) {
+        	
+        } else if (casa_de_banho >= 3) {
+        	
+        } else {
+        	
+        }
+        if (comer >= 5) {
+        	
+        } else if (comer >= 6) {
+        	
+        } else {
+        	
+        }
+        if (jogar >= 5) {
+        	
+        } else if (jogar >= 6) {
+        	
+        } else {
+        	
+        }
+        if (tomar_banho >= 8) {
+        	
+        } else if (tomar_banho >= 10) {
+        	
+        } else {
+        	
+        }
+        if (dormir >= 24) {
+            basic.showIcon(IconNames.Asleep)
+        }
     } else {
-    	
-    }
-    if (comer >= 5) {
-    	
-    } else if (comer >= 6) {
-    	
-    } else {
-    	
-    }
-    if (jogar >= 5) {
-    	
-    } else if (jogar >= 6) {
-    	
-    } else {
-    	
-    }
-    if (tomar_banho >= 8) {
-    	
-    } else if (tomar_banho >= 10) {
-    	
-    } else {
-    	
-    }
-    if (dormir >= 24) {
-    	
+        basic.clearScreen()
     }
 })
